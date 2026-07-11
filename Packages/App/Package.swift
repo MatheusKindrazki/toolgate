06a965ebbc10a0b421ee1ad867bcde9c37371045
Matheus Kindrazki <matheuskindrazki@gmail.com>
2026-07-11 08:48:08 -0300

    fix(ci): support macos 14 Swift toolchain

--- Packages/App/Package.swift
+++ Packages/App/Package.swift
@@ -1,3 +1,3 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 import PackageDescription
 let package = Package(name: "ToolgateApp", platforms: [.macOS(.v14)], products: [.executable(name: "ToolgateApp", targets: ["ToolgateApp"])], targets: [.executableTarget(name: "ToolgateApp")])
